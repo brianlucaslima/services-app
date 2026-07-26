9c7e5e82e4c0ee5b2eb636a616d3649bdd79886b
Brian Lucas <brianlucas67@gmail.com>
2026-07-26 20:56:46 +0100

    remove mixedCase

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -39,7 +39,7 @@ class AppServiceProvider extends ServiceProvider
 
         Password::defaults(fn (): ?Password => app()->isProduction()
             ? Password::min(8)
-                ->mixedCase()
+                // ->mixedCase()
                 ->letters()
                 ->numbers()
                 // ->symbols()
